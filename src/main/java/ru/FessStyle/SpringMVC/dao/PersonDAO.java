6b078fcf7ed59c17c486e1daabe52093a972b7c5
Evgeny Mereha <skyflex713@gmail.com>
2021-02-26 20:01:32 +0700

    CRUD, REST, Паттерн DAO (Data Access Object)

---
+++ src/main/java/ru/FessStyle/SpringMVC/dao/PersonDAO.java
@@ -0,0 +1,33 @@
+package ru.FessStyle.SpringMVC.dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import ru.FessStyle.SpringMVC.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Eugene Mereha
+ */
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
